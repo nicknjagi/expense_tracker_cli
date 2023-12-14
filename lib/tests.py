8f661b5db9cf231fcbfb344c503bcff21d25aa98
agent19music <seanmotanya@gmail.com>
2023-12-14 13:42:10 +0300

    Add tests

--- lib/tests.py
+++ lib/tests.py
@@ -2,6 +2,7 @@
 
 from config import session
 from models import User, Expense, Category
+from sqlalchemy import func
 
 def create_user(first_name , last_name , phone_number ):
     first_name = first_name
@@ -70,6 +71,48 @@ def create_expense(amount, description, user_id, category):
     print('')
     return expense
 
+def list_user_expenses(id):
+    user = session.query(User).filter(User.id == id).first()
+    print(f'\nExpenses for {user.first_name} {user.last_name}\n')
+    
+    expenses = user.expenses
+    for expense in expenses:
+        print(expense)
+    print('')
+    
+def user_expenses_categories(id):
+    user = session.query(User).filter(User.id == id).first()
+    print(f'\nExpense categories for {user.first_name} {user.last_name}')
+    
+    categories = user.categories
+    for category in categories:
+        print(category)
+    print('')
+    
+def get_expenses():
+    expenses = session.query(Expense).limit(20).all()
+    
+    for expense in expenses:
+        print(expense)       
+    print('')
+    
+def user_category_spending(user_id):
+    category_amount = (
+        session.query(Category.name, func.sum(Expense.amount))
+        .join(Expense)
+        .filter(Expense.user_id == user_id)
+        .group_by(Category.name)
+        .all()
+        )
+    user = session.query(User).filter(User.id == user_id).first()
+    print(f'\nCategory wise spending for {user.first_name} {user.last_name}')
+    print('')
+    for category, amount in category_amount:
+        bar = '#' * int(amount / 15)
+        print(f"Category: {category:15}, Total Spending: {amount} {bar}")
+    print('')
+
+
         
 if __name__ == "__main__":
     print("\n>>>>>>>>>>>> USER OPERATIONS <<<<<<<<<<<\n")
@@ -96,4 +139,16 @@ if __name__ == "__main__":
     
     print(f"{'*' * 6} Creating new expense {'*' * 6}")
     expense = create_expense(100, 'bought a toothbrush', 5, 'utilities')
+
+    print(f"{'' * 6} getting first 20 expenses {'' * 6}")
+    get_expenses()
+    
+    print(f"{'*' * 6} Listing all user expenses {'*' * 6}")
+    list_user_expenses(7)
+    
+    print(f"{'*' * 6} Getting all expenses for a user {'*' * 6}")
+    user_expenses_categories(8)
+
+    print(f"{'*' * 6} Getting category wise spending for user {'*' * 6}")
+    user_category_spending(8)
     
